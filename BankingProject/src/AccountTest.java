977e5522d94a004e52843158f43ddbc45d111984
muhammetsahin2003 <muhammetsahin2003@gmail.com>
2025-05-21 17:52:40 -0400

    third

--- BankingProject/src/AccountTest.java
+++ BankingProject/src/AccountTest.java
@@ -2,7 +2,7 @@ public class AccountTest {
     public  static void main(String[] args){
         Account account1 = new Account(10000, "John Doe", "123456789");
         Account account2 = new Account(20000, "Jane Doe", "987654321");
-        System.out.println("Account1 Name....: "+ account1.getName() + " \nAccount1 balance: "+account1.getBanance()+" \nAccount1 account number: "+account1.getAccountNumber());
+        System.out.println("Account1 Name....vvvvvv: "+ account1.getName() + " \nAccount1 balance: "+account1.getBanance()+" \nAccount1 account number: "+account1.getAccountNumber());
 
 
     }
