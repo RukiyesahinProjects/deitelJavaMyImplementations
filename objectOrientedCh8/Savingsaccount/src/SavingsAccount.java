466ce69c99b903e5c266a22baf43b0a2b374d753
muhammetsahin2003 <muhammetsahin2003@gmail.com>
2025-06-01 00:25:34 -0400

    third456655

---
+++ objectOrientedCh8/Savingsaccount/src/SavingsAccount.java
@@ -0,0 +1,12 @@
+public class SavingsAccount {
+    private static double annualInterestRate;
+    private double savingsBalance;
+
+    public SavingsAccount(double initialBalance) { }
+
+    public void calculateMonthlyInterest() { }
+
+    public static void modifyInterestRate(double newRate) { }
+
+    public double getSavingsBalance() { return 0.0; }
+}
